92100f896db07152c31c5b28bacd0fbf14c8370b
MAJOR <jklee007@mymail.sim.edu.sg>
2023-01-05 15:45:20 +0800

    fix letter spacing

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
           <h1 className="font-bold text-[22px] leading-7 text-[#1F314F]">
             Improve your front-end skills by building projects
           </h1>
-          <p className="w-[250px] font-normal text-[15px] leading-[19px] text-[#7D889E] p-4 pb-0">
+          <p className="w-[250px] font-normal text-[15px] leading-[19px] tracking-[0.1875px] text-[#7D889E] m-4 mb-0">
             Scan the QR code to visit Frontend Mentor and take your coding
             skills to the next level
           </p>
